0dcf7543e7d3feeead7534980f516f122fefbf4a
JB-Tellez <jb.tellez@gmail.com>
2022-08-08 11:35:45 -0700

    adds aloha starter

---
+++ api/aloha.py
@@ -0,0 +1,16 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
+        message = self.path
+        
+        # message = f"Aloha {friend}"
+        self.wfile.write(message.encode())
+        return
